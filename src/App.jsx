6c8454a7cce2bbf88bbaab51a15dbd9cf46909fb
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-09 19:53:12 +1000

    Initial Commit
    Uploading project files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Content from "./components/Content";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Content />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,8 @@
+export default function Content() {
+  return (
+    <div>
+      <h1>Content Component</h1>
+      <p>Goes here...</p>
+    </div>
+  );
+}
