54fba0156b6bf6a35f5737e97b342a8efce19b2d
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-10 20:28:58 +1000

    feat: added styling to content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,14 @@
 export default function Content() {
   return (
-    <div>
-      <h2>Fun facts about React</h2>
-      <ul>
+    <main>
+      <h2 className="main-title">Fun facts about React</h2>
+      <ul className="facts-list">
         <li>Was first realease in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 200K stars on GitHub</li>
         <li>Is maintained by Meta</li>
         <li>Powers thousand of enterprise apps, including mobile apps</li>
       </ul>
-    </div>
+    </main>
   );
 }
