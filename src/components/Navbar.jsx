86c4a53354cd43c3b662125901b05dccbc6139c4
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-10 19:53:00 +1000

    feat: added styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,9 @@ import Logo from "../assets/ReactFacts-icon.png";
 export default function Navbar() {
   return (
     <nav>
-      <img src={Logo} alt="Image of React Icon" />
-      <h3>ReactFacts</h3>
+      <img className="nav-logo" src={Logo} alt="Image of React Icon" />
+      <h3 className="nav-logo-title">ReactFacts</h3>
+      <h4 className="push-right">React Course - Project 1</h4>
     </nav>
   );
 }
